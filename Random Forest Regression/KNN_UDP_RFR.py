bdd627a8cef13e21c374086d7705518643b0bd1a
Sigma Jahan <86054663+SigmaJahan@users.noreply.github.com>
2022-05-24 21:14:14 -0300

    Add files via upload

---
+++ Random Forest Regression/KNN_UDP_RFR.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the benign dataset
+dataset_benign = pd.read_csv('benign_traffic.csv', delimiter=',')
+def zerolistmaker(n):
+    listofzeros = [0] * n
+    return listofzeros
+attack = zerolistmaker(175240)
+dataset_benign['Attack'] = attack
+dataset_benign.head()
+
+#Importing the Attack dataset (UDP)
+dataset_attack = pd.read_csv('udp.csv', delimiter=',')
+def onelistmaker(n):
+    listofones = [1] * n
+    return listofones
+attack = onelistmaker(217034)
+dataset_attack['Attack'] = attack
+dataset_attack.head()
+
+#Concat
+df_udp = pd.concat([dataset_benign, dataset_attack])
+X = df_udp.drop('Attack',1)
+y = df_udp['Attack']
+
+
+
+#RFR
+from sklearn.ensemble import RandomForestRegressor
+rfr=RandomForestRegressor(random_state=0, max_depth=10)
+rfr.fit(X,y)
+
+features = df_udp.columns 
+importances = rfr.feature_importances_ 
+indices = np.argsort(importances)[-14:]  # top 15 features 
+plt.title('Feature Importances') 
+plt.barh(range(len(indices)), importances[indices], color='b', align='center') 
+plt.yticks(range(len(indices)), [features[i] for i in indices]) 
+plt.xlabel('Relative Importance') 
+plt.show()
+df_udp.head()
+
+from sklearn.feature_selection import SelectFromModel 
+feature = SelectFromModel(rfr,threshold=0.25)
+Fit = feature.fit_transform(X,y)
+X=Fit
+
+#Splitting the test train sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+#Normalizing
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+# Fitting classifier to the Training set
+from sklearn.neighbors import KNeighborsClassifier as kp
+classifier = kp(n_neighbors = 5, metric = 'minkowski', p =2)
+classifier.fit(X_train, y_train)
+
+#Predicting the Test set
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+
+
